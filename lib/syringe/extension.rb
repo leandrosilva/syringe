18fddf9ac229bea83b3d347beba8c6a712537f4a
Leandro Silva <leandrodoze@gmail.com>
2010-03-16 15:44:42 -0700

    now instance variable also created for each injected object

--- lib/syringe/extension.rb
+++ lib/syringe/extension.rb
@@ -2,10 +2,11 @@
 # Extension for Object to provide dependency injection capability.
 #
 class Object
+  # Define a new method and instance variable named as +object_name+ parameter.
   def self.inject(object_name)
     instance_eval do
       send(:define_method, object_name) do
-        Syringe::Container.default[object_name]
+        instance_variable_set("@#{object_name}", Syringe::Container.default[object_name])
       end
     end
   end
